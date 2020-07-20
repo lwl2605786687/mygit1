e44e45ec58e7b95f650d3b586e0381187639e698
lwl <lwl@it.com>
2020-07-20 09:24:44 +0800

    3.0-4.0

--- src/com/mygit/Main.java
+++ src/com/mygit/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("1.0");
         System.out.println("2.0");
+        System.out.println("3.0");
     }
 }
