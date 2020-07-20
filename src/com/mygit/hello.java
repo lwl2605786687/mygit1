c87d46dddce1f4f575df1c4a097a370b6cc81613
lwl <lwl@it.com>
2020-07-20 09:17:09 +0800

    3.0-4.0

---
+++ src/com/mygit/hello.java
@@ -0,0 +1,13 @@
+package com.mygit;
+
+import java.sql.SQLOutput;
+
+/**
+ * @author LWL
+ * @create 2020-07-20 9:16
+ */
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("4.0");
+    }
+}
